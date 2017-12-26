62c78968c5ad2effda2b35902036bda11181650d
Nadeem Ansari <s524968@mail.nwmissouri.edu>
2017-12-26 12:58:26 -0600

    Add html, css and js files to project.

---
+++ index.js
@@ -0,0 +1,30 @@
+/* var cxr_template = {
+    "annotations": [
+        {
+            columns: [
+                { "title": "Major Anatomic Regions", "value": "Lungs" },
+                { "title": "Subanatomy", "value": "Upper lung zone" },
+                { "title": "Laterality Modifier", "value": "Peripheral" },
+                { "title": "Findings", "value": "Alveolar Opacity" },
+                { "title": "Character Modifiers", "value": "No, faint, moderately dense,  very dense" },
+                { "title": "Number of anomalies", "value": "NA" },
+                { "title": "Size Modifiers", "value": "Small, Medium, Large" }
+            ]
+        }
+    ]
+}; */
+
+// ANNOTATION CONTROLLER
+var annotationController = (function () {
+    // Some code
+})();
+
+// UI CONTROLLER
+var UIController = (function () {
+    // Some code
+})();
+
+// GLOBAL APP CONTROLLER
+var controller = (function (){
+    // Some code
+})(annotationController, UIController);
