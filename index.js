39df48bfc9feb8ad1d4549b42a1e205a223d5a3c
Nadeem Ansari <s524968@mail.nwmissouri.edu>
2018-01-01 07:28:54 -0600

    Add code to create table and table headings.
    Also added html to create input fields for modifiers.

--- index.js
+++ index.js
@@ -1,36 +1,119 @@
-/* var cxr_template = {
-    "annotations": [
-        {
-            columns: [
-                { "title": "Major Anatomic Regions", "value": "Lungs" },
-                { "title": "Subanatomy", "value": "Upper lung zone" },
-                { "title": "Laterality Modifier", "value": "Peripheral" },
-                { "title": "Findings", "value": "Alveolar Opacity" },
-                { "title": "Character Modifiers", "value": "No, faint, moderately dense,  very dense" },
-                { "title": "Number of anomalies", "value": "NA" },
-                { "title": "Size Modifiers", "value": "Small, Medium, Large" }
-            ]
-        }
-    ]
-}; */
-
 // ANNOTATION CONTROLLER
-var annotationController = (function () {
-    // Some code
+var AnnotationCtrl = (function () {
+    var cxr_template = {
+        "col": [
+            "Major Anatomic Regions",
+            "Subanatomy",
+            "Laterality Modifier",
+            "Location Modifier",
+            "Findings",
+            "Character Modifiers",
+            "Number of anomalies",
+            "Size Modifiers"
+        ],
+        "annotation-rows": [
+            {
+                row: [
+                    { "col-title": "Major Anatomic Regions", "value": "Lungs" },
+                    { "col-title": "Subanatomy", "value": "Upper lung zone" },
+                    { "col-title": "Laterality Modifier", "value": "Bilateral" },
+                    { "col-title": "Location Modifier", "value": "Peripheral" },
+                    { "col-title": "Findings", "value": "Alveolar Opacity" },
+                    { "col-title": "Character Modifiers", "value": "No, faint, moderately dense,  very dense" },
+                    { "col-title": "Number of anomalies", "value": "NA" },
+                    { "col-title": "Size Modifiers", "value": "Small, Medium, Large" }
+                ]
+            },
+            {
+                row: [
+                    { "col-title": "Major Anatomic Regions", "value": "Lungs" },
+                    { "col-title": "Subanatomy", "value": "Upper lung zone" },
+                    { "col-title": "Laterality Modifier", "value": "Bilateral" },
+                    { "col-title": "Location Modifier", "value": "Central" },
+                    { "col-title": "Findings", "value": "Alveolar Opacity" },
+                    { "col-title": "Character Modifiers", "value": "No, faint, moderately dense,  very dense" },
+                    { "col-title": "Number of anomalies", "value": "NA" },
+                    { "col-title": "Size Modifiers", "value": "Small, Medium, Large" }
+                ]
+            },
+            {
+                row: [
+                    { "col-title": "Major Anatomic Regions", "value": "Lungs" },
+                    { "col-title": "Subanatomy", "value": "Upper lung zone" },
+                    { "col-title": "Laterality Modifier", "value": "Bilateral" },
+                    { "col-title": "Location Modifier", "value": "None" },
+                    { "col-title": "Findings", "value": "Alveolar Opacity" },
+                    { "col-title": "Character Modifiers", "value": "No, faint, moderately dense,  very dense" },
+                    { "col-title": "Number of anomalies", "value": "NA" },
+                    { "col-title": "Size Modifiers", "value": "Small, Medium, Large" }
+                ]
+            },
+        ]
+    };
+
+    return {
+        getColumnNames: function () {
+            return cxr_template.col;
+        },
+        getRows: function () {
+            return cxr_template['annotation-rows'];
+        }
+    }
 })();
 
 // UI CONTROLLER
-var UIController = (function () {
-    // Some code
+var UICtrl = (function () {
+
+    var body, table, thead, tbody, tr, td;
+
+    var DOMstrings = {
+        body: 'body',
+        table: 'table',
+        thead: 'thead',
+        th: 'th',
+        tr: 'tr',
+        td: 'td'
+    };
+
+    var createTblHeading = function (colNames) {
+        body = document.getElementsByTagName(DOMstrings.body)[0];
+        table = document.createElement(DOMstrings.table);
+        thead = document.createElement(DOMstrings.thead);
+        tr = document.createElement(DOMstrings.tr);
+    
+        for (var i = 0; i < colNames.length; i++) {
+            var th = document.createElement(DOMstrings.th);
+            var cellText = document.createTextNode(colNames[i]);
+            th.appendChild(cellText);
+            tr.appendChild(th);
+        }
+    
+        thead.appendChild(tr);
+        table.appendChild(thead);
+        table.classList.add('table');
+        table.classList.add('table-bordered');
+        body.appendChild(table);
+    };
+
+    return {
+        displayTbl: function (colNames, rows) {
+            createTblHeading(colNames);
+            
+        },
+    };
 })();
 
 // GLOBAL APP CONTROLLER
-var controller = (function (){
+var controller = (function () {
 
-    // var ctrlAddAnnotation = function () {
-    //     console.log('It works');
-    // }
+    return {
+        init: function () {
+            console.log('Application started');
+            UICtrl.displayTbl(AnnotationCtrl.getColumnNames(), AnnotationCtrl.getRows());
+            $('.table').DataTable();
+        }
+    }
 
-    // document.querySelector('#btn-save').addEventListener('click', ctrlAddAnnotation());
+})(AnnotationCtrl, UICtrl);
 
-})(annotationController, UIController);
+controller.init();
